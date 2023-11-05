a22d15b6fbdabf945732f85060168f50d57ce98c
amezianechayer <amezianechayer@github.com>
2023-11-05 06:15:16 +0100

    feat: :lock: Make secured and protected route
    Added Clerk auth

---
+++ app/(racine)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const PageRacine = () => {
+    return (
+      <div className="p-4">
+        route protegé
+      </div>
+    )
+  }
+
+  export default PageRacine
