deede33d4ab0eb3a6118d65b26ae5167244fe9bb
amezianechayer <amezianechayer@github.com>
2023-11-06 16:51:05 +0100

    feat: :construction: Added the (Créer une Boutique)

--- components/modeles/modele-boutique.tsx
+++ components/modeles/modele-boutique.tsx
@@ -1,13 +1,22 @@
 "use client";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import axios from "axios";
 
 import { utiliserBoutiqueModel } from "@/brancher/util-model-boutique";
 import { Model } from "@/components/ui/model";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, 
+    FormControl,
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage 
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+
 
 const formSchema = z.object({
     name: z.string().min(1),  /* au moins un caractère suffit pour nommer la boutique */
@@ -17,6 +26,7 @@ export const ModeleBoutique = () => {
     /* controler si la boutique est ouverte ou fermée */
     const modelBoutique = utiliserBoutiqueModel();
 
+    const [loading, setLoading] = useState(false);
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -25,8 +35,16 @@ export const ModeleBoutique = () => {
     });
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-        // TODO: Creer la boutique
+        try {
+          setLoading(true)
+
+          const response = await axios.post('/api/boutiques', values);
+          console.log(response.data);
+        } catch(error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
     }
     return (
     <Model 
@@ -47,15 +65,19 @@ export const ModeleBoutique = () => {
                             <FormItem>
                                 <FormLabel>Nom</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="Chic-Blend" {...field} />
+                                    <Input disabled={loading} placeholder="Chic-Blend" {...field} />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
                         )} 
                         />
                         <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                            <Button variant="outline" onClick={modelBoutique.onClose}>Fermer</Button>
-                            <Button type="submit">Continuer</Button>
+                            <Button 
+                            disabled= {loading}
+                            variant="outline" 
+                            onClick={modelBoutique.onClose}>
+                                Fermer</Button>
+                            <Button disabled= {loading} type="submit">Continuer</Button>
                         </div>
                     </form>
                 </Form>
