f5c1ad8c62e0417f4fe7d092adbdee62ddf60501
amezianechayer <amezianechayer@github.com>
2023-11-08 06:01:31 +0100

    feat: :sparkles: Add a settings form and a save changes button

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,7 +1,86 @@
-export const SettingsForm = () => {
+"use client";
+
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+
+interface SettingsFormProps {
+    initialData: Store;
+}
+
+const formSchema = z.object({
+    name: z.string().min(1)
+    
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+export const SettingsForm: React.FC<SettingsFormProps> = ({
+    initialData
+}) => {
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    });
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data);
+    };
+
     return (
-        <div>
-            Setting Form
+       <>
+        <div className="flex items-center justify-between">
+            <Heading
+              title="Settings"
+              description="Gérer les préférences du magasin"  
+            
+            />
+            <Button 
+            disabled={loading}
+            variant="destructive"
+            size="sm"
+            onClick={() => setOpen(true)}
+            
+            >
+            <Trash className="h-4 w-4"/>
+            </Button>
         </div>
+        <Separator />
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                <div className="grid grid-cols-3 gap-8">
+                    <FormField 
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Nom</FormLabel>
+                            <FormControl>
+                                <Input disabled={loading} placeholder="Nom de la Boutique" {...field}/>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    
+                    />
+                </div>
+                <Button disabled={loading} className="ml-auto" type="submit">
+                Sauvegarder les modifications
+                </Button>
+            </form>
+        </Form>
+        </>
     );
 };
