5b9135984cfb5eb3bbc2ed0ee6ff88c53b986547
amezianechayer <amezianechayer@github.com>
2023-11-09 15:48:29 +0100

    feat: :sparkles: Add the API form and alert

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -15,6 +15,8 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import { useOrigin } from "@/hooks/use-origin";
 
 
 interface SettingsFormProps {
@@ -33,6 +35,9 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
 }) => {
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
+
+
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -118,6 +123,11 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
                 </Button>
             </form>
         </Form>
+        <Separator />
+        <ApiAlert 
+        title="TEST_TECHNIQUE_THE_BRADEY_API_URL" 
+        description={`${origin}/api/${params.storeId}`} 
+        variant="public"/>
         </>
     );
 };
